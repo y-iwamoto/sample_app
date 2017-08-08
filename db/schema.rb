3ffb6acfbb9b4d16af947d09243b8e43858bd207
岩本雄貴 <klc.oka.iwa.tsu.gmiw@gmail.com>
2017-08-08 21:44:02 +0000

    Make a Micropost model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170808210533) do
+ActiveRecord::Schema.define(version: 20170808213309) do
+
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
