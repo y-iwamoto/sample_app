96e2455681d724893d74c831d4b0a1b0122bcfae
岩本雄貴 <klc.oka.iwa.tsu.gmiw@gmail.com>
2017-08-10 21:58:51 +0000

    Make a Relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,26 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                     class_name:  "Relationship",
+                                     dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :name, presence:true, length:{maximum: 50}  
   def feed
     Micropost.where("user_id = ?", id)
+  end
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
   end
 end
