5e66e6b925fe26cdc2e8017049a6feddea5014ba
岩本雄貴 <klc.oka.iwa.tsu.gmiw@gmail.com>
2017-08-10 23:43:28 +0000

    Finish user follow / unfollow actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
   end
-  
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
   private
     def admin_user
       redirect_to(root_path) unless current_user.admin?
